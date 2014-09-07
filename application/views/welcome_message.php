8d3cab500ecdc69579a88742e9c9d145e893d494
Clifford <Greenclifford30@gmail.com>
2014-09-06 22:40:57 -0500

    Foundation 5 test 7(autoload script)

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <?php Assets::css('foundation.css', 'normalize.css'); ?>
+    <?php Assets::css(array('foundation.css', 'normalize.css')); ?>
     <title>Welcome to CodeIgniter</title>
 
     <style type="text/css">
