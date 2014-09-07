66e31c25d70b1c8214d34669a8b4733c6f1d9d10
Clifford <Greenclifford30@gmail.com>
2014-09-06 20:29:09 -0500

    configured auto-load

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('email', 'encrypt', 'form_validation', 'table', 'image_lib', 'javascript', 'jquery', 'security', 'session', 'typography', 'user_agent');
 
 
 /*
