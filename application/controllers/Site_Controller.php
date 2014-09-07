29f0868b93b24ef517fcec13ed8991d89353c1c1
Clifford <Greenclifford30@gmail.com>
2014-09-07 14:47:54 -0500

    change in controller

--- application/controllers/Site_Controller.php
+++ application/controllers/Site_Controller.php
@@ -1,6 +1,6 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller
+class Site_Controller extends CI_Controller
 {
 
     /**
@@ -20,10 +20,10 @@ class Welcome extends CI_Controller
      */
     public function index()
     {
-        $this->load->view('welcome_message');
+        $this->load->view('index');
 
     }
 }
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
+/* End of file Site_Controller.php */
+/* Location: ./application/controllers/Site_Controller.php */
