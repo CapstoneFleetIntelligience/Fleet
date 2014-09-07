6b10298b90955d59173068b3404f211167e6d72d
Clifford <Greenclifford30@gmail.com>
2014-09-06 23:10:39 -0500

    reset to working application

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -92,7 +92,6 @@
 
     <div id="body">
         <p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-        <?php echo button_anchor('#', 'Working Button', 'Submit'); ?>
 
         <p>If you would like to edit this page you'll find it located at:</p>
         <code>application/views/welcome_message.php</code>
