a29d4de924d738f567f90ba9e0bffd5d3bad9540
Greenclifford30 <Greenclifford30@gmail.com>
2014-09-25 11:36:31 -0500

    change to home view

--- application/views/home.php
+++ application/views/home.php
@@ -1,6 +1,23 @@
 <div class="container">
-
     <div class="row">
-
+        <div class="contain-to-grid sticky">
+            <nav class="top-bar" data-topbar role="navigation" data-options="sticky_on: large">
+                <ul class="title-area">
+                <li class="name">
+                    <h1><a href="#">Home</a></h1>
+                </li>
+                </ul>
+                <ul class="inline-list">
+                    <li> 
+                        <a href=""></a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </div>
-</div>
+    <div class="row">
+        <div class="block">
+
+        </div>
+    </div>
+</div
