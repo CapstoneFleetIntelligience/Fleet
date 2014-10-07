75de205ddb238aea0b9e4a398284524c25217934
Brian Cole <bcolemutech@hotmail.com>
2014-10-07 17:04:51 -0500

    misc update (adding map)

--- application/views/adminH.php
+++ application/views/adminH.php
@@ -5,8 +5,23 @@
  * Date: 9/26/14
  * Time: 8:50 PM
  */
-?>
+    $results=$this->db->get_where('capsql.business',array('bname'=> $this->session->userdata('bname')));
 
+?>
+<script>
+    function initialize()
+    {
+        var mapProp = {
+            center:new google.maps.LatLng(51.508742,-0.120850),
+            zoom:5,
+            mapTypeId:google.maps.MapTypeId.ROADMAP
+        };
+        var map=new google.maps.Map(document.getElementById("googleMap")
+            ,mapProp);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+</script>
 <div class="container">
     <div class="row">
         <div class="large-12 medium-12 small-12 columns">
@@ -27,8 +42,7 @@
             </div>
         </div>
         <div class="large-9 medium-9 small-12 columns">
-            <div class="panel">
-                <h2 style="text-align: center;">Recent Deliveries</h2>
+            <div id="googleMap" style="width:500px;height:380px;">
             </div>
         </div>
     </div>
