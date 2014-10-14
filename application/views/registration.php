25c41aed4062877b7052bc5444ed15251a0508d2
KNicholson <knn4kd@mail.missouri.edu>
2014-10-14 18:08:00 -0500

    Updated registration form
    made changes to the registration form

--- application/views/registration.php
+++ application/views/registration.php
@@ -7,81 +7,91 @@
  */
 ?>
 
-<div class="container">
-    <div class="row">
-        <?php echo form_open('register'); ?>
-        <div class="row">
-            <div class="small-6 small columns">
-                <span class="prefix">
-                    Business name
-                </span>
-                <?php echo form_input('name'); ?>
-            </div>
-            <div class="small-6 small columns">
-                <span class="prefix">
-                     Address
-                </span>
-                <?php echo form_input('baddress'); ?>
-            </div>
-        </div>
-        <div class="row">
-            <div class="small-6 small columns">
-            <span class="prefix">
-                    Phone
-                </span>
-                <?php echo form_input('bphone'); ?>
-            </div>
-            <div class="small-3 small columns">
-            <span class="prefix">
-                    Max Delivery Range
-                </span>
-                <?php echo form_input('radius'); ?>
-            </div>
-            <div class="small-3 small columns">
-            <span class="prefix">
-                    Max items allowed to deliver
-                </span>
-                <?php echo form_input('capacity'); ?>
-            </div>
-        </div>
-        <div class="row">
-            <div class="small-8 small-centered columns">
-            <span class="prefix">
-                Business Default Password
-                </span>
-                <?php echo form_password('dpass'); ?>
-            </div>
-        </div>
-        <div class="row">
-            <div class="small-6 small columns">
-                <span class="prefix">
-                    Manager name
-                </span>
-                <?php echo form_input('uname'); ?>
-            </div>
-            <div class="small-6 small columns">
-                <span class="prefix">
-                     Email
-                </span>
-                <?php echo form_input('email'); ?>
-            </div>
-        </div>
-        <div class="row ">
-            <div class="small-6 small columns">
-            <span class="prefix">
-                    Password
-                </span>
-                <?php echo form_password('pass'); ?>
-            </div>
-            <div class="small-6 small columns">
-            <span class="prefix">
-                    Phone
-                </span>
-                <?php echo form_input('uphone'); ?>
-            </div>
-        </div>
-        <?php echo form_submit('', 'Finish', "class ='button small'");
-        echo form_close();
-        ?>
-    </div>
+
+<div class="row">
+	<form>
+		<div class="row">
+			<div class="small-10 medium-12 large-12 columns">
+				<fieldset>
+					<legend>Please fill-out the following fields</legend>
+						
+						<div class="row">
+							<div class="small-5 medium-6 large-6 columns">
+								<label>Business Name
+									<input type="text" name="name">
+								</label>
+							</div>
+							<div class="small-5 medium-6 large-6 columns">
+								<label>Business Address
+									<input type="text" name="baddress">
+								</label>
+							</div>
+						</div>
+						
+						<div class="row">
+							<div class="small-4 medium-5 large-6 columns">
+								<label>Business Phone Number
+									<input type="text" name="bphone">
+								</label>
+							</div>
+							<div class="small-2 medium-3 large-3 columns">
+								<label>Max Delivery Range
+									<input type="text" name="radius">
+								</label>
+							</div>
+							<div class="small-4 medium-4 large-3 columns">
+								<label>Max Items Allowed to Deliver
+									<input type="text" name="capacity">
+								</label>
+							</div>
+						</div>
+						
+						<div class="row">
+							<div class="small-8 small-centered columns">
+								<label>Default Business Password
+									<input type="text" name="dpass">
+								</label>
+							</div>
+						</div>
+						
+						<br />
+						<hr />
+						<br />
+						
+						<div class="row">
+							<div class="small-5 medium-6 large-6 columns">
+								<label>Manager Name
+									<input type="text" name="uname">
+								</label>
+							</div>
+							<div class="small-5 medium-6 large-6 columns">
+								<label>Email
+									<input type="text" name="email">
+								</label>
+							</div>
+						</div>
+						
+						<div class="row">
+							<div class="small-5 medium-6 large-6 columns">
+								<label>Create Password
+									<input type="text" name="pass">
+								</label>
+							</div>
+							<div class="small-5 medium-6 large-6 columns">
+								<label>Phone
+									<input type="text" name="uphone">
+								</label>
+							</div>
+						</div>
+					
+				</fieldset>
+				
+				<br />
+				
+				<div class="small-3 columns">
+					<a role="button" aria-label="submit form" tabindex="0" class="button">Finish</a>
+				</div>
+			</div>
+		</div>
+	</form>
 </div>
