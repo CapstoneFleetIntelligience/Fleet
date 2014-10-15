20893ac00c515aebf32cea6cc654a04c238794d6
knn4kd_11088 <knn4kd@babbage.cs.missouri.edu>
2014-10-14 20:23:58 -0500

    misc updates

---
+++ application/views/registration.php
@@ -0,0 +1,227 @@
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: student
+ * Date: 9/21/14
+ * Time: 7:17 PM
+ */
+?>
+
+<<<<<<< HEAD
+
+<div class="row">
+	<form>
+		<div class="row">
+			<div class="small-10 medium-12 large-12 columns">
+				<fieldset>
+					<legend>Please fill-out the following fields</legend>
+						
+						<div class="row">
+							<div class="small-5 medium-6 large-6 columns">
+								<label>Business Name
+									<input type="text" name="name">
+								</label>
+							</div>
+							<div class="small-5 medium-6 large-6 columns">
+								<label>Business Address
+									<input type="text" name="baddress">
+								</label>
+							</div>
+						</div>
+						
+						<div class="row">
+							<div class="small-4 medium-5 large-6 columns">
+								<label>Business Phone Number
+									<input type="text" name="bphone">
+								</label>
+							</div>
+							<div class="small-2 medium-3 large-3 columns">
+								<label>Max Delivery Range
+									<input type="text" name="radius">
+								</label>
+							</div>
+							<div class="small-4 medium-4 large-3 columns">
+								<label>Max Items Allowed to Deliver
+									<input type="text" name="capacity">
+								</label>
+							</div>
+						</div>
+						
+						<div class="row">
+							<div class="small-8 small-centered columns">
+								<label>Default Business Password
+									<input type="text" name="dpass">
+								</label>
+							</div>
+						</div>
+						
+						<br />
+						<hr />
+						<br />
+						
+						<div class="row">
+							<div class="small-5 medium-6 large-6 columns">
+								<label>Manager Name
+									<input type="text" name="uname">
+								</label>
+							</div>
+							<div class="small-5 medium-6 large-6 columns">
+								<label>Email
+									<input type="text" name="email">
+								</label>
+							</div>
+						</div>
+						
+						<div class="row">
+							<div class="small-5 medium-6 large-6 columns">
+								<label>Create Password
+									<input type="text" name="pass">
+								</label>
+							</div>
+							<div class="small-5 medium-6 large-6 columns">
+								<label>Phone
+									<input type="text" name="uphone">
+								</label>
+							</div>
+						</div>
+					
+				</fieldset>
+				
+				<br />
+				
+				<div class="small-3 columns">
+					<a role="button" aria-label="submit form" tabindex="0" class="button">Finish</a>
+				</div>
+			</div>
+		</div>
+	</form>
+</div>
+=======
+<div class="container">
+    <div class="row">
+        <?php echo form_open('register', 'data-abide'); ?>
+            <div class="row">
+                <div class="small-6 small columns">
+                    <div class="name-field">
+                    <label><small>required</small></label>
+                    <span class="prefix">
+                    Business Name
+                    </span>
+                    <input type="text" required pattern="[a-zA-Z]+" name="name">
+                    <small class="error">Name is required and must be a string.</small>
+                    </div>
+                    <?php //echo form_input('name'); ?>
+                </div>
+                <div class="small-6 small columns">
+                    <label><small>required</small></label>
+                    <span class="prefix">
+                     Business Address
+                    </span>
+                    <input type="text" required pattern="[a-zA-Z0-9]+" name="baddress">
+                    <small class="error">An address is required and must be a string.</small>
+                    <?php //echo form_input('baddress'); ?>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="small-6 small columns">
+                    <label><small>required</small></label>
+                    <span class="prefix">
+                    Phone
+                    </span>
+                    <input type="text" required pattern="[0-9]+" name="bphone">
+                    <small class="error">A phone number is required.</small>
+                    <?php //echo form_input('bphone'); ?>
+                </div>
+                <div class="small-3 small columns">
+                    <label><small>required</small></label>
+                    <span class="prefix">
+                    Max Delivery Range
+                    </span>
+                    <input type="text" required pattern="[1-9]+" name="radius">
+                    <small class="error">A delivery range is required.</small>
+                    <?php //echo form_input('radius'); ?>
+                </div>
+                <div class="small-3 small columns">
+                    <label><small>required</small></label>
+                    <span class="prefix">
+                    Max Items Allowed to Deliver
+                    </span>
+                    <input type="number" required pattern="[1-9]+" name="capacity">
+                    <small class="error">The capacity amount is required.</small>
+                    <?php //echo form_input('capacity'); ?>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="small-8 small-centered columns">
+                    <label><small>required</small></label>
+                    <span class="prefix">
+                    Business Default Password
+                    </span>
+                    <input type="password" required pattern="[a-zA-Z0-9]+" name="dpass">
+                    <small class="error">A password is required.</small>
+                    <?php //echo form_password('dpass'); ?>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="small-6 small columns">
+                    <label><small>required</small></label>
+                    <span class="prefix">
+                    Manager Name
+                    </span>
+                    <input type="text" required pattern="[a-zA-Z]+" name="uname">
+                    <small class="error">A Manager Name is required and must be a string.</small>
+                    <?php //echo form_input('uname'); ?>
+                </div>
+                <div class="small-6 small columns">
+                    <div class="email-field">
+                    <label><small>required</small></label>
+                    <span class="prefix">
+                     Email
+                    </span>
+                    <input type="email" name="email" required>
+                    <small class="error">An email address is required.</small>
+                    <?php //echo form_input('email'); ?>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row ">
+                <div class="small-6 small columns">
+                    <label><small>required</small></label>
+                    <span class="prefix">
+                    Password
+                    </span>
+                    <input type="password" required pattern="[a-zA-Z0-9]+" name="pass">
+                    <small class="error">A password is required.</small>
+                    <?php //echo form_password('pass'); ?>
+                </div>
+                <div class="small-6 small columns">
+                    <label><small>required</small></label>
+                    <span class="prefix">
+                    Phone
+                    </span>
+                    <input type="text" required pattern="[0-9]+" name="uphone">
+                    <small class="error">A phone number is required.</small>
+                    <?php //echo form_input('uphone'); ?>
+                </div>
+            <button type="submit">Finish</button>
+            </form>
+            </div>
+        <?php //echo form_submit('', 'Finish', "class ='button small'");
+        echo form_close();
+        ?>
+    </div>
+</div>
+
+<body>
+
+<script>
+    $(document).foundation();
+</script>
+
+</body>
+>>>>>>> c136a5e84a065c9eb75cd08765111268e93d69b4
---
+++ application/views/templates/header.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Clifford
+ * Date: 9/17/14
+ * Time: 9:32 PM
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <?php echo link_tag('assets/css/main.css'); ?>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBsl9m5vNRyfN_82WPuUUDpycK6FjwcPEY">
+    </script>
+
+    <?php echo link_tag('assets/css/foundation.css'); ?>
+    <?php echo link_tag('assets/css/normalize.css'); ?>
+    <?php echo script_tag('assets/js/vendor/jquery.js'); ?>
+    <?php echo script_tag('assets/js/vendor/modernizr.js'); ?>
+    <?php echo script_tag('assets/js/vendor/jquery.cookie.js'); ?>
+    <?php echo script_tag('assets/js/vendor/fastclick.js'); ?>
+    <?php echo script_tag('assets/js/foundation/foundation.js'); ?>
+    <?php echo script_tag('assets/js/foundation/foundation.tab.js'); ?>
+    <?php echo script_tag('assets/js/foundation/foundation.abide.js'); ?>
+    <?php echo script_tag('assets/js/foundation/foundation.reveal.js'); ?>
+    <?php echo script_tag('assets/js/foundation/foundation.topbar.js'); ?>
+    <?php //echo script_tag('assets/js/maps.google.polygon.containsLatLng.js'); ?>
+    <title><?php echo $title ?></title>
+</head>
+
+<body>
+<<<<<<< HEAD
+	<div class="sticky">
+		<nav class="top-bar" data-topbar role="navigation" data-options="sticky_on: large"> 
+			<ul class="title-area"> 
+				<!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone --> 
+				<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li> 
+			</ul> 
+			<section class="top-bar-section"> 
+				<!-- Left Nav Section --> 
+				<ul class="left"> 
+					<li><?php echo anchor('adminH', 'Home'); ?></li>
+					<li><?php echo anchor('analytics', 'Analytics') ?></li>
+					<li><?php echo anchor('adminE', 'Edit')?></li> 
+					
+				</ul>
+			</section>
+		</nav>
+	</div>
+
+	<br />
+=======
+<div class="row header">
+    <div class="small-6 columns">
+        <h1 class="center">
+            Welcome to Fleet Intelligence
+        </h1>
+        </div>
+    <div class="small-6 columns">
+        <?php $role = $this->session->userdata('role');
+        //var_dump($role);
+                if(!($role))
+                {
+                    echo anchor('registration', 'please register', array('class' => 'right'));
+                    echo anchor('login', 'Log in', array('class' => 'button small radius right'));
+                }
+                else
+                {
+                    echo anchor('logout', 'logout', array('class' => 'button small radius right'));
+                }
+
+        ?>
+    </div>
+</div>
+<div class="row">
+    <dl class="small-12 tabs text-center" data-options="sticky_on: large" data-tab>
+        <?php
+                switch ($role)
+                {
+                    case 'E':
+                        echo '<dd class="active">'.anchor('overview', 'Home').'</dd>';
+                        echo '<dd>'.anchor('assignments', 'Route Assignment').'</dd>';
+                        echo '<dd>'.anchor('contact', 'contact').'</dd>';
+                        break;
+                    case 'M':
+                        echo '<dd class="active">'. anchor('adminH', 'Home').'</dd>';
+                        echo '<dd>'.anchor('analytics', 'Analytics').'</dd>';
+                        echo '<dd>'.anchor('manE', 'Edit');
+                        break;
+                    case 'A':
+                        echo '<dd class="active">'. anchor('adminH', 'Home').'</dd>';
+                        echo '<dd>'.anchor('analytics', 'Analytics').'</dd>';
+                        echo '<dd>'.anchor('adminE', 'Edit').'</dd>';
+                        break;
+                    default:
+                        break;
+                }
+        ?>
+            </dl>
+</div>
+>>>>>>> c136a5e84a065c9eb75cd08765111268e93d69b4
+
+	<div class="row header">
+		<div class="small-4 medium-6 large-6 columns">
+			<h1>Fleet Intelligence</h1>
+		</div>
+		<div class="small-5 columns">
+			<ul class="button-group right">
+				<li><?php echo anchor('login', 'Log In', array('class' => 'button')) ?></li>
+				<li><?php echo anchor('registration', 'Register', array('class' => 'button')) ?></li>
+			</ul>
+		</div>
+	</div>
+	
+	<!--
+	<div class="row">
+		<dl class="small-12 tabs text-center" data-options="sticky_on: large" data-tab>
+					<dd class="active"><?php echo anchor('adminH', 'Home'); ?></dd>
+					<dd><?php echo anchor('analytics', 'Analytics') ?></dd>
+					<dd><?php echo anchor('adminE', 'Edit')?></dd>
+		</dl>
+	</div>
+	//-->
+	
+</body>
+</html>
+	
