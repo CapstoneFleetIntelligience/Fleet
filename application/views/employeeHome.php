dccb5550426fa788eafc5f41111640d4ef8a0de2
Brian Cole <bpcx97@mail.missouri.edu>
2014-11-10 17:42:41 -0600

    pull data on employee home

--- application/views/employeeHome.php
+++ application/views/employeeHome.php
@@ -9,7 +9,37 @@
 $pass = array(
     'name' => 'pass',
     'id' => 'pass'
-)
+);
+//set user variables
+//$schd = date(c);
+$schd = '2014-11-11';
+$bname = $this->session->userdata('bname');
+
+//query to get route data
+$sql1 = "SELECT * FROM capsql.route WHERE bname = ? AND schd = ? AND uname = ?";
+$result1 = $this->db->query($sql1,array($bname,$schd,$user->uname));
+//get total distance
+$dist = 0;
+foreach ($result1->result() as $row){
+    $dist += $row->dist;
+}
+//get count of total routes
+$rcount = $result1->num_rows();
+
+//query to get delivery data
+$sql2 =  "SELECT * FROM capsql.delivery AS d, capsql.customer AS c, capsql.route AS r WHERE r.schd = d.schd AND c.cid = d.cid AND c.bname = r.bname AND d.schd = ? AND c.bname = ? AND r.uname = ?";
+$result2 = $this->db->query($sql2, array($schd,$bname,$user->uname));
+//get count of all deliveries
+$dcount = $result2->num_rows();
+
+//query to get delivery item data
+$sql3 = "SELECT i.qty FROM capsql.delivery AS d, capsql.customer AS c, capsql.route AS r, capsql.del_item AS i WHERE r.schd = d.schd AND c.cid = d.cid AND c.bname = r.bname AND i.cid = c.cid AND d.schd = ? AND c.bname = ? AND r.uname = ?";
+$result3 = $this->db->query($sql3,array($schd,$bname,$user->uname));
+//get total delivery items
+$icount = 0;
+foreach ($result3->result() as $row){
+    $icount += $row->qty;
+}
 ?>
 <?php if($user->pass == $business->dpass): ?>
 <div id="password" class="row">
@@ -31,7 +61,7 @@ $pass = array(
     </div>
     <div class="row">
         <div class="small-offset-4 small-centered">
-            <p> Todays Route Assignment</p>
+            <p> Today's Route Assignment</p>
         </div>
     </div>
 
