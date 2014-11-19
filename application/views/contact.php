ca506522ef8f447ccf72dd4d47dfdef7ac1f76e3
Clifford <Greenclifford30@gmail.com>
2014-11-19 16:24:39 -0600

    Browser platform identification.

--- application/views/contact.php
+++ application/views/contact.php
@@ -5,17 +5,32 @@
  * Date: 10/20/14
  * Time: 6:16 PM
  */
+
+$url =  '<a href="http://www.google.com/maps/dir/Current+Location/'.$business->baddress.'" target="_blank"
+rel="external">'.$business->baddress.'</a>';
 ?>
 
 <div class="container">
     <div class="row">
         <div class="small-centered">
+            <?php
+            if($this->agent->is_mobile())
+            {
+                $platform = $this->agent->platform();
+                //echo $platform;
+                if($platform=='Linux') $url =  '<a href="geo://0,0?q='.$business->baddress.'" data-rel="external">'
+                    .$business->baddress.'</a>';
+                elseif($platform=='Mac OS X')  $url =  '<a href="geo://0,0?q='.$business->baddress.'" data-rel="external">'.$business->baddress.'</a>';
+                else $url = '<a href="maps:'.$business->baddress.'">'.$business->baddress.'</a>';
+            }
+            ?>
+
             <h2 class="text-center"><?php echo $business->name ?></h2>
             <b><h4 class="text-center">Phone Number:</b> <?php echo '<a href="tel:'.$business->bphone.'">'
                     .$business->bphone
                     .'</a>' ?>
             </h4>
-            <b><h4 class="text-center">Address:</b> <?php echo '<a href="https://www.google.com/maps/dir/Current+Location/'.$business->baddress.'" target="_blank" rel="external">'.$business->baddress.'</a>' ?>
+            <b><h4 class="text-center">Address:</b> <?php echo $url ?>
             </h4>
         </div>
     </div>
