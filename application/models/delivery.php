365a7d9cb24ffac6d9d2900adbbe393a964044ac
Clifford <Greenclifford30@gmail.com>
2014-11-30 18:03:47 -0600

    Current and future dates on deliveries

--- application/models/delivery.php
+++ application/models/delivery.php
@@ -1,11 +1,11 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Bcolemutech
  * Date: 10/1/14
  * Time: 11:11 PM
  */
-
 class delivery extends CI_Model
 {
     public $cid;
@@ -23,8 +23,7 @@ class delivery extends CI_Model
      */
     public function setDelv($data)
     {
-        foreach($data as $key => $value)
-        {
+        foreach ($data as $key => $value) {
             $this->$key = $value;
         }
     }
@@ -36,11 +35,10 @@ class delivery extends CI_Model
     public function getDeliveries()
     {
         $business = $this->session->userdata('bname');
-        $dsql = "select * from delivery as d, customer as c where d.cid = c.cid and c.bname = ?";
-        $dquery = $this->db->query($dsql, array($business));
+        $dsql = "select * from delivery as d, customer as c where d.cid = c.cid and c.bname = ? and d.schd >=? ";
+        $dquery = $this->db->query($dsql, array($business, date("Y-m-d")));
         $customer = array();
-        foreach($dquery->result() as $index => $delivery)
-        {
+        foreach ($dquery->result() as $index => $delivery) {
             $cust = new delivery();
             $cust->cid = $delivery->cid;
             $cust->schd = $delivery->schd;
@@ -49,39 +47,33 @@ class delivery extends CI_Model
             $cust->name = $delivery->cname;
             $cust->iname = array();
             $cust->qty = array();
-            if($delivery->cid)
-            {
-                $this->db->select('qty, iname, ischk');
-                $this->db->from('del_item');
-                $this->db->where(array('cid' => $delivery->cid));
-                $this->db->join('chkitem', 'del_item.iid = chkitem.iid');
-                $query = $this->db->get();
-                foreach($query->result() as $key => $del_item)
-                {
-                    $cust->iname[$key] = $del_item->iname;
-                    $cust->qty[$key] = $del_item->qty;
-                    $cust->ischk = $del_item->ischk;
-                }
+
+            $this->db->select('qty, iname, ischk');
+            $this->db->from('del_item');
+            $this->db->where(array('cid' => $delivery->cid, 'ischd >=' => date("Y-m-d")));
+            $this->db->join('chkitem', 'del_item.iid = chkitem.iid');
+            $query = $this->db->get();
+            foreach ($query->result() as $key => $del_item) {
+                $cust->iname[$key] = $del_item->iname;
+                $cust->qty[$key] = $del_item->qty;
+                $cust->ischk = $del_item->ischk;
             }
             $customer[$index] = $cust;
         }
-            return $customer;
+        return $customer;
     }
 
     public function getCompleted()
     {
         $deliveries = $this->getDeliveries();
         $date = getdate();
-        $date = $date['year'].'-'.$date['mon'].'-'.$date['mday'];
+        $date = $date['year'] . '-' . $date['mon'] . '-' . $date['mday'];
         $deliveryCount = 0;
-        foreach($deliveries as $delivery)
-        {
-            if($delivery->schd == $date)
-            {
-                if($delivery->ischk = 't') $deliveryCount++;
+        foreach ($deliveries as $delivery) {
+            if ($delivery->schd == $date) {
+                if ($delivery->ischk = 't') $deliveryCount++;
                 else continue;
-            }
-            else continue;
+            } else continue;
         }
         return $deliveryCount;
     }
@@ -102,7 +94,7 @@ class delivery extends CI_Model
      */
     public function insert()
     {
-        if($this->db->insert('delivery', $this));
+        if ($this->db->insert('delivery', $this)) ;
         else throw new Exception('Failed to insert');
     }
 }
